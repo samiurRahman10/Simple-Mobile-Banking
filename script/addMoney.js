04513c7ae041549970b1e2bd8047a63f8ba2734b
samiurRahman10 <soikot1197@gmail.com>
2025-08-28 02:56:37 +0600

    Transaction Part Added

--- script/addMoney.js
+++ script/addMoney.js
@@ -1,4 +1,5 @@
 const actualPin = 1234;
+const transactionData = [];
 // Function for get input number
 function getInputNumber(id) {
     return parseInt(document.getElementById(id).value);
@@ -27,7 +28,16 @@ function toggle(id) {
     }
     document.getElementById(id).style.display = 'block'
 }
-
+// Function for toggle style
+function toggle2(id) {
+    const cardBtns = document.getElementsByClassName('card-btn');
+    for (const cardBtn of cardBtns) {
+        cardBtn.classList.remove("border-[#0874f2]", "bg-[#0874f20d]");
+        cardBtn.classList.add("border-gray-300");
+    }
+    document.getElementById(id).classList.remove("border-gray-300");
+    document.getElementById(id).classList.add("border-[#0874f2]", "bg-[#0874f20d]");
+}
 // Add Money Functionalities
 document.getElementById('add-money-btn').addEventListener('click', function (e) {
     e.preventDefault();
@@ -43,6 +53,12 @@ document.getElementById('add-money-btn').addEventListener('click', function (e) 
     const availableBalance = getInnerText('available-balance');
     const totalBalance = availableBalance + addMoneyAmount;
     setInnerText(totalBalance);
+
+    const data = {
+        name: "Add Money",
+        date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data);
 })
 
 // Cash Out Functionalities 
@@ -64,6 +80,11 @@ document.getElementById('Withdraw-money-btn').addEventListener('click', function
         alert('Insufficient Balance');
     }
 
+    const data = {
+        name: "Cash Out",
+        date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data);
 })
 
 // Transfer Money functionalities
@@ -85,7 +106,42 @@ document.getElementById('transfer-money-btn').addEventListener('click', function
         const newTotalMoney = availableBalance - amount;
         setInnerText(newTotalMoney);
     }
+
+    const data = {
+        name: "Transfer Money",
+        date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data);
 })
+// Transaction Feature
+document.getElementById("transaction-money-card").addEventListener('click', function () {
+    // Where To Add
+    const parent = document.getElementById("transaction-money-content-container");
+    parent.innerText= '';
+
+    for (const data of transactionData) {
+        // What to add
+        const div = document.createElement('div');
+        div.innerHTML = ` 
+        <div class="bg-white p-3 rounded-lg flex justify-between items-center mb-3">
+                    <div class="flex items-center gap-3">
+                        <div class="p-2 rounded-full bg-[#f4f5f7]">
+                            <img src="./assets/wallet1.png" alt="">
+                        </div>
+                        <div>
+                            <h1 class="font-bold">${data.name}</h1>
+                            <p class="text-[#666768]">${data.date} </p>
+                        </div>
+                    </div>
+
+                    <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
+                </div>
+               `
+            // Add The child
+               parent.appendChild(div);
+    }
+}
+)
 // Log Out Feature
 document.getElementById('logOut').addEventListener('click', function () {
     window.location.href = "./index.html";
@@ -94,12 +150,19 @@ document.getElementById('logOut').addEventListener('click', function () {
 // Toggle Feature
 document.getElementById('add-money-card').addEventListener('click', function () {
     toggle('money-add-form');
+    toggle2('add-money-card');
 })
 
 document.getElementById('withdraw-money-card').addEventListener('click', function () {
     toggle('money-withdraw-form');
+    toggle2('withdraw-money-card');
 })
 
 document.getElementById('transfer-money-card').addEventListener('click', function () {
-    toggle('money-transfer-form')
+    toggle('money-transfer-form');
+    toggle2('transfer-money-card');
+})
+
+document.getElementById('transaction-money-card').addEventListener('click', function () {
+    toggle('money-transaction-form');
 })
